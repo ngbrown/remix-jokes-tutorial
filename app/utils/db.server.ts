c03cbdc8e8bdda1610b3e943d72d2ffc2141482f
Nathan Brown <nathan@nkbrown.us>
2022-05-17 23:11:59 -0700

    Use createClient() pattern in global export.

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,21 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-let db: PrismaClient;
+export const db: PrismaClient = createClient();
 
 declare global {
+  // noinspection ES6ConvertVarToLetConst
   var __db: PrismaClient | undefined;
 }
 
 // this is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
-if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
-} else {
-  if (!global.__db) {
-    global.__db = new PrismaClient();
+function createClient() {
+  if (process.env.NODE_ENV === "production") {
+    return new PrismaClient();
+  } else {
+    if (!global.__db) {
+      global.__db = new PrismaClient();
+    }
+    return global.__db;
   }
-  db = global.__db;
 }
-
-export { db };
